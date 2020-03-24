26328663fa4d6a455a3bb183adf9415657f46803
John Robertson <john.goceljak@gmail.com>
2020-03-23 20:22:55 -0400

    9 done

--- src/Exercises.java
+++ src/Exercises.java
@@ -149,6 +149,20 @@ public class Exercises {
 	  if (list == null || list.length == 0) {
           return null;
       }
+	  if (ascending) {
+          for (int i = 0; i < list.length - 1; i++) {
+              int index = i;
+              for (int j = i + 1; j < list.length; j++) {
+                  if (list[j] < list[index]) {
+                      index = j;
+                  }
+              }
+              int smallerNumber = list[index];
+              list[index] = list[i];
+              list[i] = smallerNumber;
+          }
+      }
+      return list;
   }
 
   public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
